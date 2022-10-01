8d7d579c1fd586d439844fa36def38ff930ce05a
Isaac Cruz <cruzisaac51@gmail.com>
2022-10-01 16:37:56 -0500

    week 3 and teamwork

---
+++ week3/js/main.js
@@ -0,0 +1,86 @@
+// View Object
+const view = {
+    score: document.querySelector('#score strong'),
+    question: document.getElementById('question'),
+    result: document.getElementById('result'),
+    info: document.getElementById('info'),
+    render(target,content,attributes) {
+        for(const key in attributes) {
+            target.setAttribute(key, attributes[key]);
+        }
+        target.innerHTML = content;
+    }
+};
+
+const quiz = [
+    { name: "Superman",realName: "Clark Kent" },
+    { name: "Wonder Woman",realName: "Diana Prince" },
+    { name: "Batman",realName: "Bruce Wayne" },
+];
+
+const game = {
+    start(quiz){
+        this.questions = [...quiz];
+        this.score = 0;
+        // main game loop
+        for(const question of this.questions){
+        this.question = question;
+        this.ask();
+        }
+        // end of main game loop
+        this.gameOver();
+    },
+    ask(){
+        const question = `What is ${this.question.name}'s real name?`;
+        view.render(view.question,question);
+        const response =  prompt(question);
+        this.check(response);
+        console.log(question)
+    },
+    check(response) {
+        const answer = this.question.realName;
+        if(response === answer){
+        view.render(view.result,'Correct!',{'class':'correct'});
+        alert('Correct!');
+        this.score++;
+        view.render(view.score,this.score);
+        } else {
+        view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
+        alert(`Wrong! The correct answer was ${answer}`);
+        }
+    },
+    gameOver() {
+        view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+    }
+}
+
+
+function doSomething(){
+    document.getElementById('id_truebtn').onclick = game.start(quiz);
+    const btn1 = document.getElementById('id_truebtn');
+    btn1.style.display = "none";
+}
+
+
+// notes 2 
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
+
+console.log(String.raw,'Hello\nworld');
