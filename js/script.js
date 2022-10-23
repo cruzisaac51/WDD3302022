d3b002477206644c353790c56f4807b63b6defc3
Isaac Cruz <cruzisaac51@gmail.com>
2022-10-22 22:08:11 -0500

    week 6 on index

--- js/script.js
+++ js/script.js
@@ -14,7 +14,8 @@ var weeklist = [
     {label: "Week 2", url: "week2/index.html"},
     {label: "Week 3", url: "week3/index.html"},
     {label: "week 4", url: "week4/index.html"},
-    {label: "week 5", url: "week5/index.html"}
+    {label: "week 5", url: "week5/index.html"},
+    {label: "week 6", url: "week6/index.html"}
 ]; 
 listWeeklyItems (weeklist, "weekList");
 
